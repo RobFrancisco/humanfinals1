841f5c01b80db31b0e19fd1671efec861cfb36a5
AngyMilkPewson <mattrylliepons@gmail.com>
2025-12-12 19:45:44 +0800

    second commit
    added routing

---
+++ pages/sales/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Carousel from '../carousel'
+import Image from 'next/image'
+export default function PonsChoice() {
+    return (
+      <>
+      <div style={{display: "flex", justifyContent: "center"}}>
+        <p style={{textAlign: "center"}} className="text-base font-normal text-gray-500 dark:text-gray-400"><br /><br />This is the sales<br /><br /></p>
+      </div>
+    
+      <div style={{display: "flex", justifyContent: "center"}}>
+        <Image 
+        src="/Deltarune.png" alt='goldship was here'
+        width="500" height="50"
+        loading="eager"
+        />
+      </div>
+      <div className="mx-auto max-w-2xl space-y-6">
+      </div>
+      <div>
+        <Carousel />
+      </div>
+      </>
+    )
+}
